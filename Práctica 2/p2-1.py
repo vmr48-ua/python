29634236b91cef208ce98e3f1e7795fc86212425
vmr48-ua <78732677+vmr48-ua@users.noreply.github.com>
2021-11-17 19:08:20 +0100

    Revisados y Comentados
    falta comparar con el código de Calera

--- Práctica 2/p2-1.py
+++ Práctica 2/p2-1.py
@@ -1,30 +1,55 @@
-def leer():
-    lista = list(map(int,input().split()))
-    return lista
+"""
+Diseña un programa que lea una lista de números enteros 
+separados por espacios en blanco y calcule:
 
-def media(lista):
-    suma = 0
-    tamaño = 1
-    if len(lista) > 0:
-        tamaño = len(lista)
-    for num in lista:
-        suma += num
-    return (suma/tamaño)
+a) La media de los números leídos
+b) La desviación típica muestral
+c) La moda (el valor que más veces aparece). Si hay dos 
+   o más que se repiten el mismo número de veces tu función
+   debe devolver el primero que aparezca en el vector
+   
+   !! '1 7 6 3 3 6 1 7' !! stdout=1 or stdout=3
 
-def desviación(lista):
-    suma = 0
-    med = media(lista)
-    div = 1
-    for i in range (len(lista)):
-        suma += (lista[i] - med)**2
-    if len(lista)-1 > 0:
-        div = (len(lista)-1)
-    return (suma/div)**0.5
+Debes utilizar una función para leer el vector de números, 
+y otra por cada apartado. Se deben imprimir los valores 
+redondeados a 5 cifras decimales si fuera necesario.
 
-def moda(lista):
-    mod = 0
-    if len(lista) > 0:
-        mod = max(lista, key = lista.count)
+Ejemplo:
+stdin=1 2 3 2 4
+stdout=2.4
+1.14018
+2
+"""
+
+def leer():             # Lee un vector números separados por espacios del teclado 
+    v = list(map(int,input().split()))
+    return v
+
+def media(v):           # Calcula la media de las componentes de un vector
+    suma = 0                   # inicialización
+    tamaño = 1                 # el mínimo tamaño tiene que ser 1 y no 0, ver return
+
+    if len(v) > 0:        # si el vector no está vacío, tamaño es el tamaño del vector
+        tamaño = len(v)
+    for num in v:
+        suma += num            # suma todas las componentes
+
+    return (suma/tamaño)       # si tamaño es 0 dividiría entre 0 
+
+def desviación(v):      # Calcula la desviación típica muestral de un vector
+    suma = 0                        # inicialización
+    med = media(v)              # cálculo de la media
+    div = 1                         # empieza por 1 para no dividir por 0
+    for i in range (len(v)):    
+        suma += (v[i] - med)**2 # cálculo del numerador
+    if len(v)-1 > 0:
+        div = (len(v)-1)        # cálculo del denominador
+    return (suma/div)**0.5          # división y raíz
+
+def moda(v):            # Calcula la moda de un vector
+    mod = 0             # el valor por defecto es 0
+    if len(v) > 0:  # devuelve el valor que más se repite en mod
+        mod = max(v, key = v.count)
     return mod
 
 entrada = leer()
--- Práctica 2/p2-2.py
+++ Práctica 2/p2-2.py
@@ -1,29 +1,56 @@
-def leer():
+"""
+Escribe una función que reciba dos vectores v y w de tamaño n
+(la función tendrá, por tanto, tres argumentos), y devuelva un 
+entero que indique si un vector es mayor, menor o igual que el otro. 
+Se considera que un vector v es mayor que otro w cuando la componente
+i-ésima de v es mayor que la misma componente de w y todas las 
+componentes anteriores a i son iguales en ambos vectores.
+
+Los siguientes ejemplos definen (informalmente) esta relación de
+orden entre vectores.
+
+v = (1,2,3,4,5) y w = (1,2,3,4,4)           1(v es mayor que w)
+v = (1,2,3,4,5) y w = (1,2,3,5,4)          -1(v es menor que w)
+v = (1,1,3,4,5) y w = (1,1,3,4,5)           0(v y w son iguales)
+
+Una vez hayas diseñado la función, haz un programa que lea un número
+entero n y dos vectores de n componentes cada uno (utilizando una
+función que lea un vector) y los compare utilizando la función, 
+escribiendo el resultado por pantalla.
+
+Ejemplo:
+stdin=5
+1 2 3 4 5
+1 2 3 5 4
+stdout=-1
+"""
+
+def leer():             # Lee un vector números separados por espacios del teclado 
     v = list(map(int,input().split()))
     return v
 
-def compare(n,v,w):
-    equal = 0
-    ans = 2 # debug vaule
-    sameLen = 1
-    if len(v) == len(w) == n:
+def compare(n,v,w):     # Compara los vectores de tamaño n y devuelve 1 0 ó -1 en función de cuál es mayor
+    equal = 0                   # inicialización
+    ans = 2                     # inicializo a 2 por debug
+    sameLen = 1                 # sameLen = false
+    if len(v) == len(w) == n:   # si tienen el mismo tamaño y coinciden con n sameLen = true
         sameLen = 0
-    if sameLen == 0:
-        for i in range(len(v)):
+    if sameLen == 0:            # si son iguales en tamaño
+        for i in range(len(v)): # compara cada componente i de cada vector
             if v[i] > w[i]:
-                ans = 1
+                ans = 1         # si v es mayor devuelve 1 y acaba
                 equal = 1
                 break
             elif v[i] < w[i]:
-                ans = -1
+                ans = -1        # si w es mayor devuelve -1 y acaba
                 equal = 1
                 break
-        if equal == 0:
+        if equal == 0:          # si ninguno de los dos es mayor es que son iguales
             ans = 0
     return ans
 
 n = int(input())
 
 output = compare(n,leer(),leer())
-if output != 2:
+if output != 2:         # Imprime solo si la comparación tiene sentido (debug línea 34)
     print(output)
