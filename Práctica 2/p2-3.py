09b9bc62aaf70b2a31b5b1681b6fcd339ac438f8
vmr48-ua <78732677+vmr48-ua@users.noreply.github.com>
2021-11-19 10:43:42 +0100

    Revisado, Comentado y Comparado
    done :)

--- Práctica 2/p2-3.py
+++ Práctica 2/p2-3.py
@@ -1,34 +1,47 @@
-def leer():
+"""
+Diseña un programa que lea un vector de componentes enteras y lo ordene
+de menor a mayor utilizando el siguiente método (conocido como método
+de intercambio):
+
+a) Buscar dentro del vector el mínimo e intercambiarlo con el valor 
+   situado en la primera posición. Por ejemplo, si el vector original
+   es v = (4,7,2,1,5), el mínimo es 1, y se intercambiaría con el 4, 
+   quedando v = (1,7,2,4,5).
+
+b) Volver al paso anterior avanzando una posición, y buscando el mínimo
+   a partir de la posición siguiente a la intercambiada. En el ejemplo,
+   habría que buscar el mínimo en (7,2,4,5).
+
+El proceso termina cuando se llega al final del vector (a la última
+posición). Una vez ordenado, el programa debe escribir el vector por
+pantalla. Debes utilizar funciones para leer, ordenar y escribir el 
+vector.
+
+Ejemplo:
+stdin=5 7 8 2 4 3 1
+stdout=1 2 3 4 5 7 8
+"""
+
+def leer():             # Lee un vector números separados por espacios del teclado 
     return list(map(int,input().split()))
 
-def escribir(v):
-    j = -1                                  # Empieza en -1 para que -1+1 = 0 en el último print en el caso de que el vector tenga un solo elemento
-    for i in range(len(v)-1):               # Imprime todos los elementos del vector menos el último en una sola línea
-        print('{} '.format(v[i]),end = '')
-        j = i
-    print(v[j+1])                           # Imprime el último elemento y un salto de línea
-    return 0
-
-# Funciona pero no sigue las directrices del enunciado al 100%
-""" def desplazar(v,num):
-    pos = -1                      # Debug value, siempre se sustituirá
-    for i in range(0,len(v)):     # Guarda la posición de num en pos
-        if v[i] == num:
-            pos = i
-    for j in range(pos,len(v)-1): # Copia el vector en la posición anterior a partir de pos
-        v[j] = v[j+1]
-    v.pop()                       # Borra el último elemento del vector
+def ordenar(v):         # Ordena un vector de menor a mayor por el método de intercambio
+    for i in range (len(v)-1):          
+        j = v.index(min(v[i:len(v)]))   # j contencrá la posición del mínimo de v a partir de la posición i
+        v[i],v[j] = v[j],v[i]           # intercambio el contenido de la posición i con j
     return v
 
-u = leer()                        # Vector leído
-v = list()                        # Voy guardando en v los valores de u ordenados
+def escribir(v):        # Imprime un vector separado por espacios
+    for i in v:         
+        if i != v[len(v)-1]:    # Todo v menos la última posición       
+            print(i,end = ' ')  # Imprime con espacios al final cada posición
+    print(v[len(v)-1])          # Imprime la última posición y un cambio de línea
 
-for i in range(len(u)): 
-    v.append(min(u))              # El mínimo valor de u se añade a v
-    u = desplazar(u,min(u))       # en u se elimina el mínimo valor
-escribir(v) """
+# Versiones anteriores de la función "ordenar"
+# comentar la función escribir y descomentar para probar
+# (usan de alguna forma un vector w copia de v)
 
-def ordenar(v):
+""" def ordenar(v):
     w = v.copy()
     for i in range(len(v)):
         for j in range(len(v)):     
@@ -43,6 +56,18 @@ def ordenar(v):
             if w[k] == min(w):
                 posiciónW = k
         w.pop(posiciónW)
-    return v
+    return v """
+
+""" def ordenar(v):
+    for i in range (len(v)-1):
+        w = v[::-1]
+        for _ in range(i):
+            w.pop()
+        for n in range(len(v)):
+            if v[n] == min(w):
+                j = n
+                break
+        v[i],v[j] = v[j],v[i]
+    return v """
 
 escribir(ordenar(leer()))
